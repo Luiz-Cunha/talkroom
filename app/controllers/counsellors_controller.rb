f698dc2f0637dd50558b662f22259cc033e8bc95
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-11 16:57:25 -0400

    Counsellor still cant accept the appointment, but he can see all the appointments not confirmed

--- app/controllers/counsellors_controller.rb
+++ app/controllers/counsellors_controller.rb
@@ -2,5 +2,6 @@ class CounsellorsController < ApplicationController
   def profile
     @counsellor = Counsellor.find(params[:id])
     @patients = @counsellor.users
+    @appointments = Appointment.where(confirmation: false)
   end
 end

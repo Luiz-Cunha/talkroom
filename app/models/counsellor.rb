971e292496be4e35b71e6e6f75a7455fbbaf99a4
Munick Souza <munick.freitas@hotmail.com>
2024-06-12 17:49:14 -0600

    Added seeds,map and fixed home page.

--- app/models/counsellor.rb
+++ app/models/counsellor.rb
@@ -2,6 +2,7 @@ class Counsellor < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
   has_many :messages, as: :sendable
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,4 +11,3 @@ class Counsellor < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   validates :address, presence: true
 end
-

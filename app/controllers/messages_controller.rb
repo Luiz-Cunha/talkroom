0f5d7f88781ff28a4b9840c13eeecb8c007b20f2
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-13 14:48:57 -0400

    Messages are protected from Hackers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,28 +3,43 @@ class MessagesController < ApplicationController
   def create
     if params[:client_chatroom_id].present?
       @client_chatroom = ClientChatroom.find(params[:client_chatroom_id])
-      @message = Message.new(message_params)
-      @message.chatroomable = @client_chatroom
-      @message.sendable = current_user || current_counsellor
-      @message.save
-      ClientChatroomChannel.broadcast_to(
-        @client_chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
-      )
-      head :ok
-      # All Create A Message for client chatroom
+
+      if @client_chatroom.user == current_user || @client_chatroom.counsellor == current_counsellor
+        @message = Message.new(message_params)
+        @message.chatroomable = @client_chatroom
+        @message.sendable = current_user || current_counsellor
+
+        if @message.save
+          ClientChatroomChannel.broadcast_to(
+            @client_chatroom,
+            render_to_string(partial: "message", locals: {message: @message})
+          )
+          head :ok
+        else
+          render json: { error: "Message could not be saved" }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: "Unauthorized" }, status: :unauthorized
+      end
     else
-      #create a message for the Counsellor chatroom
       @counsellor_chatroom = CounsellorChatroom.find(params[:counsellor_chatroom_id])
-      @message = Message.new(message_params)
-      @message.chatroomable = @counsellor_chatroom
-      @message.sendable = current_supervisor || current_counsellor
-      @message.save
-      CounsellorChatroomChannel.broadcast_to(
-        @counsellor_chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
-      )
-      head :ok
+      if @counsellor_chatroom.supervisor == current_supervisor || @counsellor_chatroom.counsellor == current_counsellor
+        @message = Message.new(message_params)
+        @message.chatroomable = @counsellor_chatroom
+        @message.sendable = current_supervisor || current_counsellor
+
+        if @message.save
+          CounsellorChatroomChannel.broadcast_to(
+            @counsellor_chatroom,
+            render_to_string(partial: "message", locals: {message: @message})
+          )
+          head :ok
+        else
+          render json: { error: "Message could not be saved" }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: "Unauthorized" }, status: :unauthorized
+      end
     end
   end
 

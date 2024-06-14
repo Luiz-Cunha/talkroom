cac0ab27aae3f0a103792cf5dab0179131a6df63
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-14 15:41:58 -0400

    solving conflicts with heroku

--- db/migrate/20240610195438_create_appointments.rb
+++ db/migrate/20240610195438_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[7.1]
   def change
     create_table :appointments do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :counsellor, foreign_key: true
+      t.references :counsellor, null: true, foreign_key: true
       t.datetime :schedule_time
       t.boolean :confirmation, default: false
       t.text :symptom

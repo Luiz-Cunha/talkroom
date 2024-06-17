3f6759c42cf6e22f964379591f15de2a4a19ff1c
Munick Souza <munick.freitas@hotmail.com>
2024-06-17 07:27:39 -0600

    Almost done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
   def profile
     @user = current_user
     @appointments = @user.appointments
+    @previous_appointments = @user.appointments.where("schedule_time < ?", Time.now)
     # This chatroom it created for presentation
     @chatroom = ClientChatroom.last
   end
-
 end

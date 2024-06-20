ba5880a5907ffbd8da75ce8d85d2d9f593e6e9af
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-20 13:17:33 -0400

    user is in the left side

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :sendable, polymorphic: true
   belongs_to :chatroomable, polymorphic: true
 
+  def sender?(sender)
+    if sendable.is_a?(Counsellor)
+      sendable.id == sender.id
+    end
+  end
+
 end

957351d3488ab20d93934607905ffbb4cdbd0553
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-23 20:56:53 -0400

    bugs fixed

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :counsellor, optional: true
-  validates :symptom, presence: true
+  validates :symptom, :schedule_time, presence: true
 
   def counsellor_fullname
     counsellor.fullname
